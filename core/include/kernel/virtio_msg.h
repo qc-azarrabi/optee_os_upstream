b101f136c7635a15a66dfc919478a4d351b112ed
Amirreza Zarrabi <amirreza.zarrabi@oss.qualcomm.com>
2026-09-09 20:58:04 -0700

    virtio: update Qualcomm copyright headers
    Signed-off-by: Amirreza Zarrabi <amirreza.zarrabi@oss.qualcomm.com>

--- core/include/kernel/virtio_msg.h
+++ core/include/kernel/virtio_msg.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2025, Linaro Limited
+ * Copyright (c) 2026, Qualcomm Technologies, Inc.
  */
 
 #ifndef __KERNEL_VIRTIO_MSG_H
--- core/include/kernel/virtio_vsock.h
+++ core/include/kernel/virtio_vsock.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2025, Linaro Limited
+ * Copyright (c) 2026, Qualcomm Technologies, Inc.
  */
 #ifndef __KERNEL_VIRTIO_VSOCK_H
 #define __KERNEL_VIRTIO_VSOCK_H
